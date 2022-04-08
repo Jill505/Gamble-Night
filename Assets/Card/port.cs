187b13ed62fa7340b2163cdb25056c1179166c09
Jill505 <orangec.org@gmail.com>
2022-04-08 21:21:42 +0800

    好耶行動v.11111.111111回回回回
    耶耶耶耶回回回回

---
+++ Assets/Card/port.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class port: MonoBehaviour
+{
+    GameObject targeter;
+    
+    void Start()
+    {
+        targeter = GameObject.Find("godsystem");
+        /*
+        if (targeter.GetComponent < "ohMyGod" > ().sezizi == 1)
+        {
+            print("好耶你成功的花式打開了遊玩模式 耶耶耶");
+        }
+
+        if (targeter.GetComponent < "ohMyGod" > ().sezizi == 1)
+        {
+            print("好耶你成功的花式打開了故事模式 耶耶耶");
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
