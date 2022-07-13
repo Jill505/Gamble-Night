4e270dba86506e12b09de36679cc8cdcd909c2ca
Jill505 <orangec.org@gmail.com>
2022-07-14 05:29:05 +0800

    Beta1.03
    更新排版
    標註素材
    新增大廳粒子

---
+++ Assets/iDontWannaAssort/toTheNet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class toTheNet : MonoBehaviour
+{
+    public void goWebSite()
+    {
+        Application.OpenURL("https://falabbogamestudio.wixsite.com/website");
+    }
+}
